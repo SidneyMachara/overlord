8b21a767d41dc7c5e9a1f2bfd0ade41dae78f7ac
SidneyMachara <sidneymachara@gmail.com>
2017-07-20 13:15:21 +0100

    login , search, register all working

--- search.php
+++ search.php
@@ -6,6 +6,9 @@ include 'includes/db_connect.php';
 
 
   $output="";
+  $email = $_SESSION['email'];
+
+
 
 
 
@@ -13,13 +16,17 @@ if(isset($_POST['search'])){
   $search_Query= $_POST['searchBox'];
 
   $search_Query = preg_replace("#[^0-9a-z]#i","",$search_Query);
-  $sql1 = "SELECT * FROM overlordtable WHERE sitename LIKE '%$search_Query%' OR sitedescription LIKE '%$search_Query%'";
+  $sql1 = "SELECT * FROM overlordtable WHERE email='$email' AND (sitename LIKE '%$search_Query%' OR sitedescription LIKE '%$search_Query%')";
+
   $result1 = mysqli_query($conn,$sql1);
 //  $count = mysql_num_rows($result1);
 
   if($result1->num_rows== 0){
     //if it does not find record bring back everythin
-    $sql1 = "SELECT * FROM overlordtable";
+    $sql1 = "SELECT overlordtable.*
+            FROM overlordtable
+            inner JOIN users on overlordtable.email = users.email
+            where overlordtable.email = '$email'";
     $result1 = mysqli_query($conn,$sql1);
     $output= $result1;
   }else{
@@ -30,7 +37,10 @@ if(isset($_POST['search'])){
 else {
   {
     //before search display everything
-   $sql1 = "SELECT * FROM overlordtable";
+    $sql1 = "SELECT overlordtable.*
+            FROM overlordtable
+            inner JOIN users on overlordtable.email = users.email
+            where overlordtable.email = '$email'";
   $result1 = mysqli_query($conn,$sql1);
    $output = $result1;
   }
